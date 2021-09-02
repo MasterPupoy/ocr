f10226be7bdcdc34fe70849e2cbab4ab8edd1954
Jerllon <jerllonstreamline@gmail.com>
2021-09-03 03:13:38 +0800

    stash
    Signed-off-by: Jerllon <jerllonstreamline@gmail.com>

---
+++ aws.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const accessKey = 'AKIAXZCDODCX2SFSTHVR';
+const secretKey = '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';
+const bucketName = 'streamline-ats-bucket';
+
+const s3 = new AWS.S3({
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey
+});
+
+module.exports = uploadFileToAws = (file) => {
+    const filename = file.filename;
+    const fileContent = fs.readFileSync(file.path);
+    const params = {
+        Bucket: bucketName,
+        Key: filename,
+        Body: fileContent,
+        ACL: 'public-read',
+    }
+
+    s3.upload(params, (err, data) => {
+        if (err) {
+            console.log('error');
+            console.log(err);
+            // reject(err)
+        } else {
+            console.log('success');
+            // resolve(data.Location)
+            console.log(data);
+        }
+    })
+}
--- routes.js
+++ routes.js
@@ -6,24 +6,26 @@ const path = require('path');
 const pdf_extract = require('pdf-extract');
 const nlp = require('./nlp_training');
 const fs = require('fs');
+const uploadFile = require('./aws');
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'uploads/');
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
   },
 
   // Change filename
-  filename: function(req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    console.log(req);
   }
 });
-const upload = multer({ storage:storage });
+const upload = multer({ storage: storage });
 
 // NLP Instance
-const corpus = JSON.parse(fs.readFileSync('./corpus.json'));
+const corpus = JSON.parse(fs.readFileSync('./corpus-sanitized.json'));
 const NLP = new nlp();
 
 const client = new MongoClient(uri, {
-  useNewUrlParser : true
+  useNewUrlParser: true
 });
 
 const testCollection = client.db('local').collection('test');
@@ -37,7 +39,7 @@ const testCollection = client.db('local').collection('test');
 
 // get applicant parsed resume data from db
 router.get('/applicant/:applicant_id', (req, res) => {
-  
+
   console.log(req.params);
 
   client.connect().then(() => {
@@ -47,12 +49,11 @@ router.get('/applicant/:applicant_id', (req, res) => {
     client.close()
   }).catch(err => console.log(err))
 
-
 })
 
 // post parsed data to db
 router.post('/applicant/:applicant_id', (req, res) => {
-  
+
   client.connect().then(() => {
     console.log('connected to mongodb')
 
@@ -62,41 +63,48 @@ router.post('/applicant/:applicant_id', (req, res) => {
 
 })
 
+router.post('/code', (req, res) => {
+  console.log(req.body);
+  res.end('code');
+})
 
-router.post('/extract-pdf', upload.single('resume'), (req, res) => {  
+
+router.post('/extract-pdf', upload.single('resume'), (req, res) => {
   // multer middleware for uploading/storing pdf first to the server disk
   // req.file contains the file information
   if (!req.file) {
-      return res.send('Please select a file to upload');
+    return res.send('Please select a file to upload');
   }
   // Initialize nlp
   NLP.initialize(corpus);
 
-  // req.file.path contains the relative path of the stored pdf
   const path = req.file.path;
+
+  // console.log(req.file);
+  uploadFile(req.file);
   // pdf-extract options or rules for parsing data from pdf
   const options = {
-    type : 'ocr',
+    type: 'ocr',
     ocr_flags: [
       '--psm 3',
       'alphanumeric'
     ]
   }
-  
+
   // Extract text from pdf
-  const processor = pdf_extract(path, options, function(err){
+  const processor = pdf_extract(path, options, function (err) {
     console.log('Processing...');
-    if(err){
+    if (err) {
       return callback(err)
     }
   });
-  
+
   // listens to pdf extraction completion
-  processor.on('complete', async function(data) {
+  processor.on('complete', async function (data) {
     // gets extracted text
     const person_data = data.text_pages;
     // loop through every page
-    const extract = async() => {
+    const extract = async () => {
       return Promise.all(person_data.map((page) => {
         // filter any new lines and empty strings
         const split = page.split('\n');
@@ -111,25 +119,26 @@ router.post('/extract-pdf', upload.single('resume'), (req, res) => {
     // result from the loop operation
     const result = await extract();
     // filter any null or undefined values
+    console.log(result);
     const final = result.map(data => {
-        return data.filter(function (el) {
-          return el != null;
-        });
+      return data.filter(function (el) {
+        return el != null;
       });
-      // deletes pdf file after process is done
-      fs.unlinkSync(path)
-      // return the final extracted and clustered data to the client
-      res.json(final);
+    });
+    // deletes pdf file after process is done
+    fs.unlinkSync(path)
+    // return the final extracted and clustered data to the client
+    res.json(final);
   });
-  
+
   // listens to pdf-extract errors
-  processor.on('err', function(err) {
+  processor.on('err', function (err) {
     inspect(err, 'error while extracting pages');
     fs.unlinkSync(path)
     res.json(err);
     return callback(err);
   })
-  
+
 })
 
 
